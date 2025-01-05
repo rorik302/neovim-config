6fa6305cd9b5ba45c460352723866deb1aae5b58
Aleksandr Danilenko <rorik302@gmail.com>
2025-01-05 22:44:03 +0300

    Исправлен формат статусбара

--- lua/plugins/nvim-ufo.lua
+++ lua/plugins/nvim-ufo.lua
@@ -26,6 +26,37 @@ return {
 		-- Убирает цифры вложенности fold в статусбаре
 		local fcs = vim.opt.fillchars:get()
 
+		local function get_lnum()
+			local cur_num
+			local sep = ","
+
+			if vim.v.virtnum ~= 0 then
+				return "-"
+			end
+
+			cur_num = vim.v.relnum == 0 and vim.v.lnum or vim.v.relnum
+
+			cur_num = tostring(cur_num):reverse():gsub("(%d%d%d)", "%1" .. sep):reverse():gsub("^,", "")
+
+			local function pad_str(in_str, width, align)
+				local ralign_token = "%="
+
+				local num_spaces = width - #in_str
+				if num_spaces < 1 then
+					num_spaces = 1
+				end
+				local spaces = string.rep(" ", num_spaces)
+
+				if align == "left" then
+					return table.concat({ in_str, spaces })
+				end
+
+				return table.concat({ spaces, in_str, ralign_token })
+			end
+
+			return pad_str(cur_num, 3, "right")
+		end
+
 		local function get_fold(lnum)
 			if vim.fn.foldlevel(lnum) <= vim.fn.foldlevel(lnum - 1) then
 				return " "
@@ -34,7 +65,7 @@ return {
 		end
 
 		_G.get_statuscol = function()
-			return "%s%l " .. get_fold(vim.v.lnum) .. " "
+			return "%s" .. get_lnum() .. " " .. get_fold(vim.v.lnum) .. " "
 		end
 
 		vim.o.statuscolumn = "%!v:lua.get_statuscol()"
