b34393206189531d87a8ac40b1cb563bb96d5be6
Aleksandr Danilenko <rorik302@gmail.com>
2025-01-07 22:30:18 +0300

    stylua вынесено к серверам

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -81,15 +81,13 @@ return {
 				},
 				mypy = {},
 				ruff = {},
+				stylua = {},
 				yamlls = {},
 			}
 
 			require("mason").setup()
 
 			local ensure_installed = vim.tbl_keys(servers or {})
-			vim.list_extend(ensure_installed, {
-				"stylua",
-			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
 			require("mason-lspconfig").setup({
